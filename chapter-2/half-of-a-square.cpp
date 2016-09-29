616b31fa7da56a1c564d903d887507f292e0db49
Jorge Rivera <jorgeriv@gmail.com>
2016-09-29 00:10:42 -0500

    First exersise chapter 2

---
+++ chapter-2/half-of-a-square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+  for(int i = 5; i > 0; i-- ){
+    for(int j = 0; j < i; j++){
+        cout << "#";
+    }
+    cout << endl;
+  }
+  return 0;
+}
